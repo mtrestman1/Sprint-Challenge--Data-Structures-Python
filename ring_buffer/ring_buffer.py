4934d7e2b50f7ba2f0681fbdf14228780f19e13b
Max Trestman <mtrestman1@gmail.com>
2019-06-14 14:49:55 -0400

    lol, realized why none was being printed.. no issue, just printing append not get

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -8,12 +8,10 @@ class RingBuffer:
     if self.current < self.capacity:
       self.storage[self.current] = item
       self.current += 1
-      return self.storage
-
+      # return self.storage
     if self.current == self.capacity:
       self.current = 0
-      return self.append(item)
-      
+      # return self.append(item)
 
   def get(self):
     this = [i for i in self.storage if i is not None]
@@ -22,8 +20,14 @@ class RingBuffer:
 
 
 shia_laBuff = RingBuffer(3)
-print(shia_laBuff.append(8) and shia_laBuff.append((1)) and shia_laBuff.append((9)))
-print(shia_laBuff.append(4))
-print(shia_laBuff.append(6))
-print(shia_laBuff.append(2))
-print(shia_laBuff.append(7))
+# print(shia_laBuff.append(8)) 
+# print(shia_laBuff.append(4))
+# print(shia_laBuff.append(6))
+# print(shia_laBuff.append(2))
+# print(shia_laBuff.append(7))
+shia_laBuff.append(8)
+shia_laBuff.append(4)
+# shia_laBuff.append(6)
+# shia_laBuff.append(2)
+# shia_laBuff.append(7)
+print(RingBuffer.get(shia_laBuff))
