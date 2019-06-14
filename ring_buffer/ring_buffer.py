e808464bcaf4f9c43a7588dcb9d7b446dc2e3f70
Max Trestman <mtrestman1@gmail.com>
2019-06-14 12:31:38 -0400

    ring_buffer tests pass, finished time complex q's

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -5,7 +5,25 @@ class RingBuffer:
     self.storage = [None]*capacity
 
   def append(self, item):
-    pass
+    if self.current < self.capacity:
+      self.storage[self.current] = item
+      self.current += 1
+      return self.storage
+
+    if self.current == self.capacity:
+      self.current = 0
+      return self.append(item)
+      
 
   def get(self):
-    pass
+    this = [i for i in self.storage if i is not None]
+    return this
+
+
+
+shia_laBuff = RingBuffer(3)
+print(shia_laBuff.append(8) and shia_laBuff.append((1)) and shia_laBuff.append((9)))
+print(shia_laBuff.append(4))
+print(shia_laBuff.append(6))
+print(shia_laBuff.append(2))
+print(shia_laBuff.append(7))
